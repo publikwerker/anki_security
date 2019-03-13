9c287bca579df19669096f5a4ad8ec42ccb895ed
Publikwerker <jason.michael.hoffman@gmail.com>
2019-03-13 10:48:55 -0700

    added apache license since i'll be using some code from github.com/anki/vector-python-sdk

--- anki_security.py
+++ anki_security.py
@@ -1,4 +1,18 @@
-"""Security system for Vector
+"""Copyright 2019 Publikwerks, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Security system for Anki Vector
 
 This is an example of using Python to direct Vector's behavior. When awakened by a loud noise, he should announce 'Documenting disturbance' and then take 10 seconds of video."""
 
